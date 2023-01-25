bc01593e351bc54a05932a86f7c4d34c1d69b09c
Nicola Toscan <nicolatoscan99@outlook.it>
2023-01-25 04:13:01 +0100

    start page

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,71 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <v-app id="inspire">
+    <!-- <v-navigation-drawer
+      model-value
+      class="pt-4"
+      color="grey-lighten-3"
+      rail
+    >
+      <v-avatar
+        v-for="n in 6"
+        :key="n"
+        :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
+        :size="n === 1 ? 36 : 20"
+        class="d-block text-center mx-auto mb-9"
+      ></v-avatar>
+    </v-navigation-drawer> -->
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <v-main class="main">
+      <div class="wrapper">
+        <h2>Welcome to</h2>
+        <h1> StudyBuddy! </h1>
+        <p> A place to study </p>
+      </div>
+    </v-main>
+  </v-app>
 
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-nav {
-  width: 100%;
-  font-size: 12px;
+export default defineComponent({
+  name: 'App'
+});
+</script>
+
+<style lang="scss">
+.main {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url("../../public/background.jpg");
+}
+.wrapper {
+  background-color: #222222C0;
+  backdrop-filter: blur(5px);
+  padding: 1.5em 4em;
+  border-radius: 1em;
+  border: 3px solid white;
+}
+h2, h1, p {
   text-align: center;
-  margin-top: 2rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+h1 {
+  font-size: 4em;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+h2 {
+  font-size: 2em;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+p {
+  border-top: 1px solid white;
+  margin-top: 2em;
+  padding: 2em;
+  font-size: 1.5em;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,30 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: { mdi }
+    },
+    theme: {
+        defaultTheme: 'dark'
+    }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
