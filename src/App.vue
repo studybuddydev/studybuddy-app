779f397311d7719fc48d4e56b894ec1c43252304
alessiogandelli <alessiogandelli99@gmail.com>
2023-02-03 03:54:46 +0100

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,85 @@
 <template>
+    <v-app id="inspire">
+        <v-app-bar app>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-toolbar-title>StudyBuddy</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text>Home</v-btn>
+            <v-btn text>About</v-btn>
+            <v-btn text>Contact</v-btn>
+        </v-app-bar>
 
-  <v-app id="inspire">
-    <!-- <v-navigation-drawer
-      model-value
-      class="pt-4"
-      color="grey-lighten-3"
-      rail
-    >
-      <v-avatar
-        v-for="n in 6"
-        :key="n"
-        :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
-        :size="n === 1 ? 36 : 20"
-        class="d-block text-center mx-auto mb-9"
-      ></v-avatar>
-    </v-navigation-drawer> -->
+        <div id="app">
+           
 
-    <v-main class="main">
-      <div class="wrapper">
-        <h2>Welcome to</h2>
-        <h1> StudyBuddy! </h1>
-        <p> Soon a place to study </p>
-      </div>
-    </v-main>
-  </v-app>
-
+            <v-navigation-drawer
+                color="#09151E"
+                permanent
+                expand-on-hover
+                app>
+              <v-divider></v-divider>
+              <v-list nav dense>
+                  <v-list-item link href="#">
+                    <v-list-item-icon> <v-icon color="white" small >mdi-lightbulb</v-icon></v-list-item-icon>
+                    <v-list-item-title class="title">Analisi</v-list-item-title>
+                  </v-list-item>
+              </v-list>
+            </v-navigation-drawer>
+     
+            <v-main class="main">
+                <div class="wrapper">
+                    <h2>Welcome to</h2>
+                    <h1>StudyBuddy!</h1>
+                    <h3>!Work in progress!</h3>
+                    <p>Soon a place to study</p>
+                </div>
+            </v-main>
+        </div>
+    </v-app>
 </template>
 
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App'
+    name: "App",
 });
 </script>
 
 <style lang="scss">
 .main {
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-image: url("/background.jpg");
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    //background-image: url("/background.jpg");
 }
 .wrapper {
-  background-color: #222222C0;
-  backdrop-filter: blur(5px);
-  padding: 1.5em 4em;
-  border-radius: 1em;
-  border: 3px solid white;
+    background-color: #222222c0;
+    backdrop-filter: blur(5px);
+    padding: 1.5em 4em;
+    border-radius: 1em;
+    border: 3px solid white;
 }
-h2, h1, p {
-  text-align: center;
+h2,
+h1,
+h3,
+p {
+    text-align: center;
 }
 h1 {
-  font-size: 4em;
+    font-size: 4em;
 }
 h2 {
-  font-size: 2em;
+    font-size: 2em;
 }
 p {
-  border-top: 1px solid white;
-  margin-top: 2em;
-  padding: 2em;
-  font-size: 1.5em;
+    border-top: 1px solid white;
+    margin-top: 2em;
+    padding: 2em;
+    font-size: 1.5em;
 }
 </style>
---
+++ src/views/StudyView.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="result-table-wrapper box">
+        <table>
+            <tr>
+                <th>N°</th>
+                <th>Nome</th>
+                <th>Media Voti</th>
+                <th>Numero voti</th>
+            </tr>
+            
+        </table>
+        <button @click="loadVotes()">Ricarica</button>
+    </div>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "StudyView",
+    data: function () {
+        return {
+            
+        };
+    },
+    components: {},
+    created: async function () {
+        
+    },
+    methods: {
+        loadVotes: async function () {
+            
+        },
+    },
+});
+</script>
+  
+  <style lang="scss">
+.result-table-wrapper {
+    margin: 1em;
+    padding: 1em;
+    display: inline-block;
+}
+table {
+    text-align: left;
+    color: white;
+    margin: 1em auto;
+    .cell-number {
+        text-align: right;
+    }
+    th,
+    td {
+        border: 1px solid white;
+        border-collapse: collapse;
+        padding: 5px;
+    }
+}
+button {
+    font-size: 1em;
+}
+</style>
