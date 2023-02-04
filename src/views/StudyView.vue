f10480510d135cb5f152a8bcbdcc9c6089b64d5f
alessiogandelli <alessiogandelli99@gmail.com>
2023-02-04 05:08:13 +0100

    add topics in exam

--- src/views/StudyView.vue
+++ src/views/StudyView.vue
@@ -1,10 +1,25 @@
 <template>
-
-    navigation drawer
-
-
-
-
+    <v-container>
+        <v-row>
+            <v-col cols="3">
+                <!-- Your small left column content goes here -->
+                <v-list dense>
+                    <v-list-item
+                        v-for="topic in topics"
+                        :key="topic.text"
+                        @click="selectTopic(topic)"
+                        >{{ topic.text }}</v-list-item
+                    >
+                </v-list>
+            </v-col>
+            <v-col cols="9">
+                <!-- Your large right column content goes here -->
+                <div v-if="selectedTopic">
+                    {{ selectedTopic.text }}
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
   
 <script lang="ts">
@@ -14,25 +29,26 @@ export default defineComponent({
     name: "StudyView",
     data: function () {
         return {
-            drawer: false,
+            topics: [
+                { text: "Topic 1" },
+                { text: "Topic 2" },
+                // ...
+            ],
+            selectedTopic: null,
         };
     },
     components: {},
-    created: async function () {
-        
-    },
+    created: async function () {},
     methods: {
-        loadVotes: async function () {
-            
+        loadVotes: async function () {},
+        selectTopic: function (topic: any) {
+            this.selectedTopic = topic;
         },
+     
     },
-    onMounted: async function () {
-        
-    },
+    onMounted: async function () {},
 });
 </script>
   
 <style lang="scss">
-
-
 </style>
