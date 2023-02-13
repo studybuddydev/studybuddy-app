53389ae29a618a0665103d2c02c5e851180e9dee
alessiogandelli <alessiogandelli99@gmail.com>
2023-02-13 18:42:57 +0100

    pausa to timer

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -5,7 +5,9 @@
                 <v-card :style="{backgroundColor: color}">
                     <v-card-title class="headline">Timer</v-card-title>
                     <v-card-text>
-                        <h1>{{ minutes }}:{{ seconds }}</h1>
+                        <h1>studio : {{ minutes }}:{{ seconds }}</h1>
+                        <p></p>
+                        <h1>pausa : {{ pminutes }}:{{ pseconds }}</h1>
                         <p>Click start to begin the timer</p>
                         <v-btn @click="startTimer">Start</v-btn>
                         <v-btn @click="stopTimer">Pause</v-btn>
@@ -29,10 +31,15 @@ export default defineComponent({
             seconds: 0,
             interval: 0,
             color: "blue",
+
+            pinterval: 0,
+            pminutes: 0,
+            pseconds: 0,
         };
     },
     methods: {
         startTimer() {
+            clearInterval(this.pinterval);
             //start timer
             this.interval = setInterval(() => {
                 if (this.seconds === 59) {
@@ -48,10 +55,25 @@ export default defineComponent({
         stopTimer() {
             clearInterval(this.interval);
             this.color = "red";
+
+            this.pinterval = setInterval(() => {
+                if (this.pseconds === 59) {
+                    this.pseconds = 0;
+                    this.pminutes += 1;
+                } else {
+                    this.pseconds += 1;
+                }
+            }, 1000);
         },
         reset() {
             this.minutes = 0;
             this.seconds = 0;
+            this.color = "blue";
+            clearInterval(this.interval);
+
+            this.pminutes = 0;
+            this.pseconds = 0;
+            clearInterval(this.pinterval);
         },
     },
 });
