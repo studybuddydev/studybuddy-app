e7ce03a5203b9b7b3aed526c9baa3f586ee92107
alessiogandelli <alessiogandelli99@gmail.com>
2023-02-13 18:39:34 +0100

    timer che cambia colore

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -2,12 +2,14 @@
     <v-container>
         <v-row class="justify-center">
             <v-col cols="12" md="8">
-                <v-card>
-                    <v-card-title class="headline">Pomodoro Timer</v-card-title>
+                <v-card :style="{backgroundColor: color}">
+                    <v-card-title class="headline">Timer</v-card-title>
                     <v-card-text>
-                        <v-time-picker v-model="time" :readonly="true">{{time/1000/60}}</v-time-picker>
+                        <h1>{{ minutes }}:{{ seconds }}</h1>
+                        <p>Click start to begin the timer</p>
                         <v-btn @click="startTimer">Start</v-btn>
-                        <v-btn @click="stopTimer">Stop</v-btn>
+                        <v-btn @click="stopTimer">Pause</v-btn>
+                        <v-btn @click="reset">Reset</v-btn>
                     </v-card-text>
                 </v-card>
             </v-col>
@@ -23,20 +25,34 @@ export default defineComponent({
     name: "PomoTimer",
     data: function () {
         return {
-            time: 25 * 60 * 1000,
-            
+            minutes: 0,
+            seconds: 0,
+            interval: 0,
+            color: "blue",
         };
     },
     methods: {
         startTimer() {
-            setInterval(() => {
-                this.time -= 1000;
-                if (this.time <= 0) {
-                    this.stopTimer();
+            //start timer
+            this.interval = setInterval(() => {
+                if (this.seconds === 59) {
+                    this.seconds = 0;
+                    this.minutes += 1;
+                } else {
+                    this.seconds += 1;
                 }
             }, 1000);
+            //change background color
+            this.color = "green";
         },
-        stopTimer() {},
+        stopTimer() {
+            clearInterval(this.interval);
+            this.color = "red";
+        },
+        reset() {
+            this.minutes = 0;
+            this.seconds = 0;
+        },
     },
 });
 </script>
