2a979dc7460c9889186c6c5ea3b5df48336484a0
alessiogandelli <alessiogandelli99@gmail.com>
2023-02-13 19:08:46 +0100

    add new exam

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,41 @@
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
             <v-toolbar-title>StudyBuddy</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-btn text>Home</v-btn>
-            <v-btn text>About</v-btn>
-            <v-btn text>Contact</v-btn>
+            <v-btn text to="/">Home</v-btn>
+            <v-btn text>Learn</v-btn>
+            <v-btn text href="https://studybuddy.it/" target="_blank"
+                >Info</v-btn
+            >
         </v-app-bar>
 
         <!-- Navigation drawer  where i can add new item, each item is an exam, the click routes to /exam -->
         <v-navigation-drawer v-model="drawer" v-if="$route.name != 'exam'" app>
             <v-list dense>
-                <v-list-item v-for="exam in exams" :key="exam.text" link :to="`/exam?name=${exam.text}`">{{ exam.text }}</v-list-item>
+                <v-list-item
+                    v-for="exam in exams"
+                    :key="exam.text"
+                    link
+                    :to="`/exam?name=${exam.text}`"
+                    >{{ exam.text }}</v-list-item
+                >
             </v-list>
-         
-        <!-- add button to the bottom with width 100% to add new item -->
-        <v-btn color="primary" width="100%" >Add new exam</v-btn>
 
-            
+            <!-- add button to the bottom with width 100% to add new item -->
+            <v-dialog v-model="dialog" width="auto">
+                <v-sheet width="300" class="mx-auto">
+                    <v-form @submit.prevent>
+                        <v-text-field
+                            v-model="examName"
+                            :rules="rules"
+                            label="Exam name"
+                        ></v-text-field>
+                        <v-btn type="submit" @click="addExam" block class="mt-2">Confirm</v-btn>
+                    </v-form>
+                </v-sheet>
+            </v-dialog>
+            <v-btn color="primary" width="100%" @click="dialog = true"
+                >Add new exam</v-btn
+            >
         </v-navigation-drawer>
 
         <!-- Sizes your content based upon application components -->
@@ -44,11 +64,25 @@ export default defineComponent({
     name: "App",
     data: function () {
         return {
-            exams: [{ text: "Analisi" }, { text: "Geometria" }, { text: "Diritto Internazionale" }],
+            exams: [
+                { text: "Analisi" },
+                { text: "Geometria" },
+                { text: "Diritto Internazionale" },
+            ],
             drawer: true,
+            dialog: false,
+            examName: "",
+            rules: [(v: string) => !!v || "Exam name is required"],
         };
     },
+    methods: {
 
+        addExam() {
+            this.exams.push({ text: this.examName });
+            this.examName = "";
+            this.dialog = false;
+        },
+    },
 });
 </script>
 
