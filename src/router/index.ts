bd5ab51ab0c7fdcb5edede9c009715ddf68bbbf1
Nicola Toscan <nicolatoscan99@outlook.it>
2023-03-05 01:39:25 +0100

    fix routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import StudyView from '@/views/StudyView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
+  
   routes: [
     {
       path: '/',
