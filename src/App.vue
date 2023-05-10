310770f6a72706b7e1aec100b25df51b928a1393
Nicola Toscan <nicolatoscan99@outlook.it>
2023-05-10 22:50:02 +0200

    pause screen

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id="inspire">
+  <v-app id="inspire" >
     <PomodoroFlex /> <!-- keep it on the top -->
     <Menu />
     <v-main class="main">
@@ -7,19 +7,30 @@
     </v-main>
     
     <CookieBanner />
+
+    <PauseScreen class="pause" transition="fade-transition" v-model="showPause" />
   </v-app>
 
 </template>
 
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import PomodoroFlex from '@/components/Pomodoro/PomodoroFlex.vue';
 import Menu from '@/components/Menus/Menu.vue';
 import CookieBanner from '@/components/Cookie/CookieBanner.vue';
+import PauseScreen from '@/components/Pause/PauseScreen.vue';
 import { useTheme } from 'vuetify'
 import { useStateStore } from "@/stores/state";
 const t = useStateStore().getTheme();
 if (t) useTheme().global.name.value = t;
+
+const showPause = ref(false);
+document.addEventListener('keyup', function (evt) {
+    if (evt.key === 'Escape') {
+      showPause.value = !showPause.value;
+    }
+});
 
 </script>
 
---
+++ src/components/Pause/PauseScreen.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-scroll-x-transition>
+    <div class="pause-screen" v-if="props.modelValue">
+      <p class="text-primary" >You are taking a</p>
+      <h2 class="text-primary" >BREAK</h2>
+
+      <ul>
+        <li @click="emit('update:modelValue', false)">Resume</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  </v-scroll-x-transition>
+</template>
+
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  modelValue: boolean,
+}>(), {})
+const emit = defineEmits(['update:modelValue'])
+
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+
+.pause-screen {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 10000;
+
+  background-color: rgba(0, 0, 0, 0.8);
+  backdrop-filter: blur(30px);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: 'Press Start 2P', cursive;
+  
+  h2 {
+    font-size: 7rem;
+  }
+  
+  p {
+    font-size: 1rem;
+  }
+
+  ul {
+    list-style: none;
+    margin-top: 2rem;
+    font-size: 1.5rem;
+
+    li {
+      margin: 0.5rem 0;
+      text-align: center;
+      cursor: pointer;
+      padding: 0.5rem;
+
+
+      &:hover {
+        color: rgb(var(--v-theme-primary));
+        text-decoration: underline;
+      }
+    }
+  }
+}
+
+</style>
