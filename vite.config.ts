85916c241ef3afbec1118c55aaa4423ae30063f9
alessiogandelli <alessiogandelli99@gmail.com>
2023-10-25 20:55:23 +0200

    PWA test

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { resolve, dirname } from "node:path";
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -13,7 +14,8 @@ export default defineConfig({
     VueI18nPlugin({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
     }),
-    vueJsx()
+    vueJsx(),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
