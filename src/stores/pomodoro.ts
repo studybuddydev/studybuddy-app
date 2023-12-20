98fb83427193e296a66cec323456386b718a4320
alessiogandelli <alessiogandelli99@gmail.com>
2023-12-20 16:37:01 +0100

    comments

--- src/stores/pomodoro.ts
+++ src/stores/pomodoro.ts
@@ -19,7 +19,7 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
   const stateStore = useStateStore();
 
   const currentPomodoro = ref(stateStore.getPomodoroStatus());
-  currentPomodoro.value = undefined
+  currentPomodoro.value = undefined                               
   let interval: number | undefined;
   let _report: PomoReport | undefined;
   let now = ref(Date.now());
@@ -30,6 +30,7 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
   function init() {
     if (!currentPomodoro.value) {
       createPomodoro();
+      console.log('creating pomo')
     } else {
       interval = setInterval(tick, TICK_TIME);
     }
@@ -77,6 +78,8 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
     _report = undefined;
   }
 
+
+  // set up pomodoro ( a study session using the data from the settings), this method modify the currentPomodoro object
   function createPomodoro() {
     clearStuff();
     const settings = settingsStore.pomodoroFlexSettings;
@@ -94,6 +97,7 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
     saveStatus();
   }
 
+  // when you first press start you set the start time of the pomodoro and sets the state to study
   function startPomodoro() {
     console.log('start')
     clearStuff();
@@ -107,6 +111,7 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
     interval = setInterval(tick, TICK_TIME);
   }
 
+  // when you press stop you set the end time of the pomodoro and sets the state to terminated
   function stopPomodoro() {
     console.log('stop')
     const pomo = getCurrentPomo();
@@ -115,7 +120,7 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
       pomo.endedAt = getNow(pomo.startedAt);
     }
   }
-
+  // between start and stop you alternate between study and pause, this method is called when you press the pause/play button during a pomdoo
   function togglePauseStudy() {
     console.log('toggle')
     const pomo = getCurrentPomo();
@@ -127,6 +132,7 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
     }
   }
 
+  // start the pause 
   function pause() {
     console.log('pause')
     const pomo = getCurrentPomo();
@@ -139,16 +145,18 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
     const now = getNow(pomo.startedAt);
     pomo.state = PomodoroState.BREAK;
 
-    const breaksLength = settingsStore.pomodoroFlexSettings.breaksLength * MINUTE_MULTIPLIER;
-    const nextPause = pomo.breaksTodo.shift();
-    if (nextPause && nextPause?.start - breaksLength > now) {
+    const breaksLength = settingsStore.pomodoroFlexSettings.breaksLength * MINUTE_MULTIPLIER;     // questa non va messa in alto come variabile globale?
+    const nextPause = pomo.breaksTodo.shift();                                                   // prendo la prossima pausa  e la levo dalla lista delle pause da fare
+    if (nextPause && nextPause?.start - breaksLength > now) {                                    //if the next pause is far enough in the future put it back in the list   
       pomo.breaksDone.push(nextPause);
     } else {
-      pomo.breaksDone.push({ start: now, end: now, soundStart: true });
+      pomo.breaksDone.push({ start: now, end: now, soundStart: true });                       // else create a new pause and put it in the list of done pauses
+      //aggiungere la pausa rosicchiata 
     }
 
   }
 
+  // start the study ending a pause 
   function study() {
     console.log('study')
     const pomo = getCurrentPomo();
@@ -159,14 +167,15 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
     adjustPomo();
 
     const now = getNow(pomo.startedAt);
-    pomo.state = PomodoroState.STUDY;
+    pomo.state = PomodoroState.STUDY;                                              // set state to study      
 
-    const lastBreak = pomo.breaksDone[pomo.breaksDone.length - 1];
+    const lastBreak = pomo.breaksDone[pomo.breaksDone.length - 1];        // get last break and set the end 
     lastBreak.end = now;
 
-    const breakTimeDone = pomo.breaksDone.reduce((acc, curr) => acc + (curr.end ?? curr.start) - curr.start, 0);
-    const nrOfBreaksDone = pomo.breaksDone.filter(b => (b.end ?? b.start) - b.start > 60*1000).length;
-
+    const breakTimeDone = pomo.breaksDone.reduce((acc, curr) => acc + (curr.end ?? curr.start) - curr.start, 0);    // get total break time done
+    const nrOfBreaksDone = pomo.breaksDone.filter(b => (b.end ?? b.start) - b.start > 60*1000).length;               // get number of breaks done
+    
+    // generate new breaks using the remaining time, the length of the breaks and the number of breaks left
     pomo.breaksTodo = generateBreaks(
       pomo.end - now,
       (settingsStore.pomodoroFlexSettings.breaksLength * MINUTE_MULTIPLIER) - breakTimeDone,
@@ -193,26 +202,28 @@ export const usePomodoroStore = defineStore('pomodoro', () => {
     // }
 
     if (pomo.state === PomodoroState.BREAK) {                                             // PAUSE
-      const currBreak = pomo.breaksDone[pomo.breaksDone.length - 1];
-      if (currBreak.end) {
-        let toSteal = now - currBreak.end;
-        if (toSteal > 0) {
-          currBreak.end = now;
-          if (!currBreak.soundEnd) {
-            currBreak.soundEnd = true;
-            playSound(ESound.BreakDone);
+      const currBreak = pomo.breaksDone[pomo.breaksDone.length - 1];    // get current break
+      if (currBreak.end) {                                              // if break is done       
+        let toSteal = now - currBreak.end;                              // time to steal from next break    
+        if (toSteal > 0) {                                              // if there is time to stea
+          currBreak.end = now;                                          // set current break end to now ( if you are in a new break you are moving the break end every second)
+          if (!currBreak.soundEnd) {                                    // if sound is not played yet   
+            currBreak.soundEnd = true;                                  // set sound played to true     
+            playSound(ESound.BreakDone);                                // play sound 
           }
         }
-        while (toSteal > 0) {
-          const nextPause = pomo.breaksTodo[0];
+        while (toSteal > 0) {                                          // while there is time to steal      
+          const nextPause = pomo.breaksTodo[0];                        // get next break    
           if (!nextPause) break;
           nextPause.start += toSteal;
+
           if (nextPause.end && nextPause.start > nextPause.end) {
             toSteal = nextPause.start - nextPause.end;
             pomo.breaksTodo.shift();
           } else {
             toSteal = 0;
           }
+
         }
       } else {
         currBreak.end = now;
