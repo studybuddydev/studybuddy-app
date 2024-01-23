76eb2b4604a3bc8b0a0bafdb134dc8ea69015b21
Nicola Toscan <nicolatoscan99@outlook.it>
2024-01-23 23:18:35 +0100

    fix pomo settings

--- src/components/Popup/Settings.vue
+++ src/components/Popup/Settings.vue
@@ -6,14 +6,14 @@
         <!-- <v-toolbar-title>Settings</v-toolbar-title> -->
         <v-tabs v-model="modelTab">
           <v-tab value="general">{{ $t("pause.general.general") }}</v-tab>
-          <v-tab value="pomodoro" :disabled="pomodoro.going">{{ $t("pause.timer.timer") }}</v-tab>
+          <v-tab value="pomodoro">{{ $t("pause.timer.timer") }}</v-tab>
           <v-tab value="theme">{{ $t("pause.theme.theme") }}</v-tab>
         </v-tabs>
       </v-toolbar>
 
       <v-card-text>
         <v-container>
-          <v-window v-model="modelTab">
+          <v-window v-model="modelTab" :touch="false">
 
             <!-- PROFILE -->
             <v-window-item value="general">
@@ -61,15 +61,17 @@
 
             <!-- POMODORO -->
             <v-window-item value="pomodoro">
+              <v-alert v-if="pomodoro.going" variant="tonal" class="mb-5" type="warning" text="To modify the timer properties you must stop it first" />
+
               <v-row>
-                <v-col cols="6" class="d-flex justify-space-around">
-                  <v-btn-toggle color="primary" group rounded="xl" variant="flat" v-model="modeSwitch">
+                <v-col cols="12" sm="6" class="d-flex justify-space-around">
+                  <v-btn-toggle color="primary" group rounded="xl" variant="flat" v-model="modeSwitch" :disabled="pomodoro.going">
                     <v-btn value="free" class="bg-background">{{ $t("pause.timer.free") }}</v-btn>
                     <v-btn value="classic" class="bg-background">{{ $t("pause.timer.classic") }}</v-btn>
                   </v-btn-toggle>
                 </v-col>
-                <v-col cols="6" class="d-flex justify-space-around">
-                  <v-btn-toggle color="primary" group variant="flat" v-model="selectedPreset" :disabled="freeMode">
+                <v-col cols="12" sm="6" class="d-flex justify-space-around">
+                  <v-btn-toggle color="primary" group variant="flat" v-model="selectedPreset" :disabled="freeMode || pomodoro.going">
                     <v-btn value="115-15-3" class="bg-background">25/5</v-btn>
                     <v-btn value="110-10-1" class="bg-background">50/10</v-btn>
                   </v-btn-toggle>
@@ -84,11 +86,11 @@
                   </div>
                 </v-col>
                 <v-col cols="3">
-                  <v-text-field v-model="endsAt" type="time" variant="underlined" dense :label= '$t("pause.timer.endTime")' :disabled="freeMode"/>
+                  <v-text-field v-model="endsAt" type="time" variant="underlined" dense :label= '$t("pause.timer.endTime")' :disabled="freeMode || pomodoro.going"/>
                 </v-col>
               </v-row>
 
-              <v-slider v-model="settingsStore.settings!.pomodoro!.totalLength" :min="0" :max="240" :step="5" thumb-label :disabled="freeMode"
+              <v-slider v-model="settingsStore.settings!.pomodoro!.totalLength" :min="0" :max="240" :step="5" thumb-label :disabled="freeMode || pomodoro.going"
                     class="pr-4" prepend-icon="mdi-timer">
                     <template v-slot:thumb-label>
                       {{ Math.floor(settingsStore.settings!.pomodoro!.totalLength / 60) }}h{{
@@ -99,16 +101,16 @@
               <div class="text-h6">{{ settingsStore.settings!.pomodoro!.breaksLength }} {{ $t("pause.timer.breaksLength")
               }}
               </div>
-              <v-slider v-model="settingsStore.settings!.pomodoro!.breaksLength" :min="1" :max="60" :step="1" thumb-label :disabled="freeMode"
+              <v-slider v-model="settingsStore.settings!.pomodoro!.breaksLength" :min="1" :max="60" :step="1" thumb-label :disabled="freeMode || pomodoro.going"
                 class="pr-4" prepend-icon="mdi-coffee" />
 
               <div class="text-h6">{{ settingsStore.settings!.pomodoro!.numberOfBreak }}
                 {{ $t("pause.timer.breaksNumber") }}</div>
-              <v-slider v-model="settingsStore.settings!.pomodoro!.numberOfBreak" :min="0" :max="10" :step="1" thumb-label :disabled="freeMode"
+              <v-slider v-model="settingsStore.settings!.pomodoro!.numberOfBreak" :min="0" :max="10" :step="1" thumb-label :disabled="freeMode || pomodoro.going"
                 show-ticks="always" class="pr-4" prepend-icon="mdi-tally-mark-5" />
 
-              <div class="text-h6">{{ $t("pause.timer.volume") }}</div>
-              <v-slider v-model="settingsStore.settings!.pomodoro!.soundVolume" :min="0" :max="100" :step="1" thumb-label :disabled="freeMode"
+              <div class="text-h6 mt-10">{{ $t("pause.timer.volume") }}</div>
+              <v-slider v-model="settingsStore.settings!.pomodoro!.soundVolume" :min="0" :max="100" :step="1" thumb-label
                 class="pr-4" :prepend-icon="volumeIcon(settingsStore.settings!.pomodoro!.soundVolume)" />
 
             </v-window-item>
@@ -178,25 +180,25 @@ const { logout } = useAuth0();
 
 
 // ----- ENDS AT
-function updateTotalLength() {
-  const date = new Date();
-  const [h, m] = endsAt.value.split(':');
-  date.setHours(+h);
-  date.setMinutes(+m);
-  const res = Math.floor((date.getTime() - Date.now()) / 1000 / 60);
-  settingsStore.settings!.pomodoro!.totalLength = res < 0 ? res + (24 * 60) : res;
-}
-function updateHoursMinutes() {
-  const date = new Date(new Date().getTime() + settingsStore.settings!.pomodoro!.totalLength*60000);
-  endsAt.value = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
-}
-const totalLength = computed(() => settingsStore.settings!.pomodoro!.totalLength)
-const endsAt = ref('00:00');
-const intUpdateTime = setInterval(() => updateHoursMinutes(), 1000 * 60);
-onUnmounted(() => clearInterval(intUpdateTime));
-updateHoursMinutes();
-watch(endsAt, () => updateTotalLength(), { deep: true });
-watch(totalLength, () => updateHoursMinutes());
+const time = ref(new Date().getTime());
+setInterval(() => time.value = new Date().getTime(), 1000 * 60);
+const endsAt = computed({
+  get () {
+    const date = new Date(time.value + settingsStore.settings!.pomodoro!.totalLength*60000);
+    console.log(date);
+    return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+  },
+  set (newValue) {
+    const date = new Date();
+    const [h, m] = newValue.split(':');
+    date.setHours(+h);
+    date.setMinutes(+m);
+    console.log('updateTotalLength', date);
+
+    const res = Math.floor((date.getTime() - Date.now()) / 1000 / 60);
+    settingsStore.settings!.pomodoro!.totalLength = res < 0 ? res + (24 * 60) : res;
+  }
+})
 
 
 // ----- MODE
@@ -218,9 +220,11 @@ const selectedPreset = computed({
   get() { 
     return `${settingsStore.settings!.pomodoro!.totalLength}-${settingsStore.settings!.pomodoro!.breaksLength}-${settingsStore.settings!.pomodoro!.numberOfBreak}`;
   },
-  set(value) { 
-    const [totalLength, breaksLength, numberOfBreak] = value.split('-');
-    presetPomo(+totalLength, +breaksLength, +numberOfBreak);
+  set(value) {
+    if (value) {
+      const [totalLength, breaksLength, numberOfBreak] = value.split('-');
+      presetPomo(+totalLength, +breaksLength, +numberOfBreak);
+    }
     
   }
 })
