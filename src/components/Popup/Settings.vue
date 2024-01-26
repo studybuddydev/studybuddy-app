4a054c4b551f4f1d3de8ffd50c0df0dd0482db6c
Nicola Toscan <nicolatoscan99@outlook.it>
2024-01-26 20:58:47 +0100

    hide timer settings when running

--- src/components/Popup/Settings.vue
+++ src/components/Popup/Settings.vue
@@ -63,51 +63,53 @@
             <v-window-item value="pomodoro">
               <v-alert v-if="pomodoro.going" variant="tonal" class="mb-5" type="warning" text="To modify the timer properties you must stop it first" />
 
-              <v-row>
-                <v-col cols="12" sm="6" class="d-flex justify-space-around">
-                  <v-btn-toggle color="primary" group rounded="xl" variant="flat" v-model="modeSwitch" :disabled="pomodoro.going">
-                    <v-btn value="free" class="bg-background">{{ $t("pause.timer.free") }}</v-btn>
-                    <v-btn value="classic" class="bg-background">{{ $t("pause.timer.classic") }}</v-btn>
-                  </v-btn-toggle>
-                </v-col>
-                <v-col cols="12" sm="6" class="d-flex justify-space-around">
-                  <v-btn-toggle color="primary" group variant="flat" v-model="selectedPreset" :disabled="freeMode || pomodoro.going">
-                    <v-btn value="115-15-3" class="bg-background">25/5</v-btn>
-                    <v-btn value="110-10-1" class="bg-background">50/10</v-btn>
-                  </v-btn-toggle>
-                </v-col>
-              </v-row>
+              <div v-if="!pomodoro.going">
+                <v-row>
+                  <v-col cols="12" sm="6" class="d-flex justify-space-around">
+                    <v-btn-toggle color="primary" group rounded="xl" variant="flat" v-model="modeSwitch" >
+                      <v-btn value="free" class="bg-background">{{ $t("pause.timer.free") }}</v-btn>
+                      <v-btn value="classic" class="bg-background">{{ $t("pause.timer.classic") }}</v-btn>
+                    </v-btn-toggle>
+                  </v-col>
+                  <v-col cols="12" sm="6" class="d-flex justify-space-around">
+                    <v-btn-toggle color="primary" group variant="flat" v-model="selectedPreset" :disabled="freeMode">
+                      <v-btn value="115-15-3" class="bg-background">25/5</v-btn>
+                      <v-btn value="110-10-1" class="bg-background">50/10</v-btn>
+                    </v-btn-toggle>
+                  </v-col>
+                </v-row>
 
-              <v-row>
-                <v-col cols="9">
-                  <div class="text-h6 text-bottom">
-                    {{ $t("pause.timer.sessionOf") }}
-                    {{ Math.floor(settingsStore.settings!.pomodoro!.totalLength / 60) }} {{ $t("pause.timer.hours") }} {{ settingsStore.settings!.pomodoro!.totalLength % 60 }} {{ $t("pause.timer.minutes") }}
-                  </div>
-                </v-col>
-                <v-col cols="3">
-                  <v-text-field v-model="endsAt" type="time" variant="underlined" dense :label= '$t("pause.timer.endTime")' :disabled="freeMode || pomodoro.going"/>
-                </v-col>
-              </v-row>
+                <v-row>
+                  <v-col cols="9">
+                    <div class="text-h6 text-bottom">
+                      {{ $t("pause.timer.sessionOf") }}
+                      {{ Math.floor(settingsStore.settings!.pomodoro!.totalLength / 60) }} {{ $t("pause.timer.hours") }} {{ settingsStore.settings!.pomodoro!.totalLength % 60 }} {{ $t("pause.timer.minutes") }}
+                    </div>
+                  </v-col>
+                  <v-col cols="3">
+                    <v-text-field v-model="endsAt" type="time" variant="underlined" dense :label= '$t("pause.timer.endTime")' :disabled="freeMode"/>
+                  </v-col>
+                </v-row>
 
-              <v-slider v-model="settingsStore.settings!.pomodoro!.totalLength" :min="0" :max="240" :step="5" thumb-label :disabled="freeMode || pomodoro.going"
-                    class="pr-4" prepend-icon="mdi-timer">
-                    <template v-slot:thumb-label>
-                      {{ Math.floor(settingsStore.settings!.pomodoro!.totalLength / 60) }}h{{
-                        settingsStore.settings!.pomodoro!.totalLength % 60 }}m
-                    </template>
-              </v-slider>
+                <v-slider v-model="settingsStore.settings!.pomodoro!.totalLength" :min="0" :max="240" :step="5" thumb-label :disabled="freeMode"
+                      class="pr-4" prepend-icon="mdi-timer">
+                      <template v-slot:thumb-label>
+                        {{ Math.floor(settingsStore.settings!.pomodoro!.totalLength / 60) }}h{{
+                          settingsStore.settings!.pomodoro!.totalLength % 60 }}m
+                      </template>
+                </v-slider>
 
-              <div class="text-h6">{{ settingsStore.settings!.pomodoro!.breaksLength }} {{ $t("pause.timer.breaksLength")
-              }}
+                <div class="text-h6">{{ settingsStore.settings!.pomodoro!.breaksLength }} {{ $t("pause.timer.breaksLength")
+                }}
+                </div>
+                <v-slider v-model="settingsStore.settings!.pomodoro!.breaksLength" :min="1" :max="60" :step="1" thumb-label :disabled="freeMode"
+                  class="pr-4" prepend-icon="mdi-coffee" />
+
+                <div class="text-h6">{{ settingsStore.settings!.pomodoro!.numberOfBreak }}
+                  {{ $t("pause.timer.breaksNumber") }}</div>
+                <v-slider v-model="settingsStore.settings!.pomodoro!.numberOfBreak" :min="0" :max="10" :step="1" thumb-label :disabled="freeMode"
+                  show-ticks="always" class="pr-4" prepend-icon="mdi-tally-mark-5" />
               </div>
-              <v-slider v-model="settingsStore.settings!.pomodoro!.breaksLength" :min="1" :max="60" :step="1" thumb-label :disabled="freeMode || pomodoro.going"
-                class="pr-4" prepend-icon="mdi-coffee" />
-
-              <div class="text-h6">{{ settingsStore.settings!.pomodoro!.numberOfBreak }}
-                {{ $t("pause.timer.breaksNumber") }}</div>
-              <v-slider v-model="settingsStore.settings!.pomodoro!.numberOfBreak" :min="0" :max="10" :step="1" thumb-label :disabled="freeMode || pomodoro.going"
-                show-ticks="always" class="pr-4" prepend-icon="mdi-tally-mark-5" />
 
               <div class="text-h6 mt-10">{{ $t("pause.timer.volume") }}</div>
               <v-slider v-model="settingsStore.settings!.pomodoro!.soundVolume" :min="0" :max="100" :step="1" thumb-label
