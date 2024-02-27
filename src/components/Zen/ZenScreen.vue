0b90cc6fb01a19d3cd71d8e1ca320f7c3fc7ff49
Nicola Toscan <nicolatoscan99@outlook.it>
2024-02-27 22:32:18 +0100

    Pip

--- src/components/Zen/ZenScreen.vue
+++ src/components/Zen/ZenScreen.vue
@@ -4,8 +4,8 @@ import { ref, computed, onMounted, onUnmounted } from 'vue';
 import { usePomodoroStore } from "@/stores/pomodoro";
 import { useSettingsStore } from "@/stores/settings";
 import minecraftSentences from '@/assets/minecraft.json';
-import PomodoroCircle from '@/components/Pomodoro/PomodoroCircle.vue';
 import PomodoroReport from '@/components/Pomodoro/PomodoroReport.vue';
+import PomodoroPip from '@/components/Pomodoro/PomodoroPip.vue';
 import Settings from '@/components/Settings/Settings.vue';
 import Info from '@/components/common/Info.vue';
 import LongAwayPopup from '@/components/Zen/LongAwayPopup.vue'
@@ -34,59 +34,6 @@ const zenStyle = computed<{ backgroundImage?: string, backgroundColor?: string }
   return {};
 });
 
-const isPipped = ref(false);
-const pipSupported = computed(() => (window as any).documentPictureInPicture);
-
-
-
-
-async function pipIt() {
-  const player = document.querySelector("#pomocirclepip");
-
-  if (!(window as any).documentPictureInPicture) return;
-
-  if ((window as any).documentPictureInPicture.window) {
-    (window as any).documentPictureInPicture.window.close();
-    isPipped.value = false;
-    return;
-  }
-
-  // Open a Picture-in-Picture window.
-  const pipWindow = await (window as any).documentPictureInPicture.requestWindow();
-
-  // Copy style sheets over from the initial document
-  // so that the player looks the same.
-  [...(document.styleSheets as any)].forEach((styleSheet) => {
-    try {
-      const cssRules = [...styleSheet.cssRules].map((rule) => rule.cssText).join('');
-      const style = document.createElement('style');
-
-      style.textContent = cssRules;
-      pipWindow.document.head.appendChild(style);
-    } catch (e) {
-      const link = document.createElement('link');
-
-      link.rel = 'stylesheet';
-      link.type = styleSheet.type;
-      link.media = styleSheet.media;
-      link.href = styleSheet.href;
-      pipWindow.document.head.appendChild(link);
-    }
-  });
-
-  // Move the player to the Picture-in-Picture window.
-  pipWindow.document.body.append(player);
-
-  // Move the player back when the Picture-in-Picture window closes.
-  pipWindow.addEventListener("pagehide", (event: any) => {
-    const playerContainer = document.querySelector("#pomocirclepipparent");
-    const pipPlayer = event.target.querySelector("#pomocirclepip");
-    playerContainer?.append(pipPlayer);
-    isPipped.value = false;
-  });
-  isPipped.value = true;
-
-}
 
 const onKeyUp = (e: KeyboardEvent) => {
   if (e.code === 'Space') {
@@ -99,7 +46,7 @@ const onKeyUp = (e: KeyboardEvent) => {
     if (!zenMode.value) {
       showPomoHistory.value = false;
     }
-    
+
   }
 };
 
@@ -116,17 +63,6 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
   <div :class="zenStyle.backgroundImage ? 'img-background' : ''">
     <Settings class="settings" v-model="openSettingsTab" />
     <LongAwayPopup />
-
-    <div v-if="pipSupported" class="hide" id="pomocirclepipparent">
-      <div id="pomocirclepip"
-        :class="zenStyle.backgroundImage ? 'pomodoro-circle-component-on-pip-wrapper-wrapper img-background' : 'pomodoro-circle-component-on-pip-wrapper-wrapper'"
-        :style="zenStyle">
-        <div class="pomodoro-circle-component-on-pip-wrapper">
-          <PomodoroCircle class="pomodoro-circle-component pomodoro-circle-component-on-pip" :in-pip="true" />
-        </div>
-      </div>
-    </div>
-
     <div transition="fade-transition">
       <v-scroll-y-reverse-transition>
         <div class="zen-screen" v-if="zenMode" :style="zenStyle">
@@ -147,12 +83,12 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
                   <h1 class="text-primary">StudyBuddy</h1>
                 </div>
                 <h6 class="text-version">v{{ appVersion }}</h6>
-                <h3 class="minecraft-sentence font-press">{{minecraftSentence}}</h3>
+                <h3 class="minecraft-sentence font-press">{{ minecraftSentence }}</h3>
               </div>
             </div>
             <!-- finish screen -->
             <div v-else-if="pomodoro.terminated && !pomodoro.going" class="blur rounded-box finish-box">
-              <v-icon class="close-icon" icon="mdi-close" @click="pomodoro.createPomodoro()"/>
+              <v-icon class="close-icon" icon="mdi-close" @click="pomodoro.createPomodoro()" />
               <div v-if="pomodoro.report?.shortPomo">
                 <p class="pause font-press text-center">{{ $t("pause.pomoDoneShort") }}</p>
                 <h3 class="text-primary font-press text-center">{{ $t("pause.goodjobShort") }}</h3>
@@ -167,14 +103,11 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
               </div>
             </div>
 
-            <div class="pomodoro-circle-component-on-zen-wrapper" v-if="
-              !isPipped && (pomodoro.countdownRunning || (pomodoro.going && (!settings.generalSettings.hideTime || pomodoro.pauseing)))
-            ">
-              <PomodoroCircle class="pomodoro-circle-component pomodoro-circle-component-on-zen" :in-pip="false" />
-              <v-btn v-if="pomodoro.going && pipSupported" density="comfortable" size="small" class="btn-pip bg-surface"
-                icon="mdi-flip-to-front" @click="pipIt()" />
-              <Info v-if="pomodoro.pauseing" :text="$t('info.pause')" class="info-pause" />
-            </div>
+            <PomodoroPip v-if="
+              (pomodoro.countdownRunning || (pomodoro.going && (!settings.generalSettings.hideTime || pomodoro.pauseing)))
+              " :zen-style="zenStyle"
+              :hide-time="settings.generalSettings.hideTime" />
+
             <!-- report table-->
             <PomodoroReport v-if="pomodoro.report" :report="pomodoro.report" />
 
@@ -183,13 +116,13 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
                 @click="showPomoHistory = true">
                 <v-icon class="icon" icon="mdi-folder-clock-outline" />
               </v-btn>
-              <v-btn class='btn bg-secondary pomo-btn pomo-box font-press btn-main-start' v-if="!pomodoro.going && !pomodoro.report?.shortPomo"
-                @click="pomodoro.startPomodoro()">
+              <v-btn class='btn bg-secondary pomo-btn pomo-box font-press btn-main-start'
+                v-if="!pomodoro.going && !pomodoro.report?.shortPomo" @click="pomodoro.startPomodoro()">
                 <span>{{ $t("pause.study") }}</span>
                 <v-icon class="icon" icon="mdi-play" />
               </v-btn>
-              <v-btn class='btn bg-secondary pomo-btn pomo-box font-press btn-main-start' v-if="!pomodoro.going && pomodoro.report?.shortPomo"
-                @click="pomodoro.createPomodoro()">
+              <v-btn class='btn bg-secondary pomo-btn pomo-box font-press btn-main-start'
+                v-if="!pomodoro.going && pomodoro.report?.shortPomo" @click="pomodoro.createPomodoro()">
                 <span>{{ $t("backHome") }}</span>
                 <v-icon class="icon" icon="mdi-home" />
               </v-btn>
@@ -198,15 +131,9 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
           </div>
         </div>
       </v-scroll-y-reverse-transition>
-
     </div>
-    <BottomBar
-      :zen-mode="zenMode"
-      :show-pomo-history="showPomoHistory"
-      @set-zen-mode="zenMode = $event"
-      @set-show-pomo-history="showPomoHistory = $event"
-      @open-settings-tab="openSettingsTab = $event"
-    />
+    <BottomBar :zen-mode="zenMode" :show-pomo-history="showPomoHistory" @set-zen-mode="zenMode = $event"
+      @set-show-pomo-history="showPomoHistory = $event" @open-settings-tab="openSettingsTab = $event" />
 
   </div>
 </template>
@@ -218,58 +145,6 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
   font-family: 'casio';
   src: url('@/assets/fonts/casio-calculator-font.ttf') format('truetype');
 }
-
-.pomodoro-circle-component-on-zen-wrapper {
-  position: relative;
-
-  .btn-pip {
-    position: absolute;
-    display: none;
-    top: 0;
-    left: 0;
-  }
-
-  
-  .info-pause {
-    top: 0;
-    right: 0;
-  }
-
-
-  &:hover .btn-pip {
-    display: block;
-  }
-
-  .pomodoro-circle-component-on-zen {
-    height: min(50vh, 80vw);
-    width: min(50vh, 80vw);
-  }
-}
-
-.pomodoro-circle-component-on-pip-wrapper-wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  background-color: rgb(var(--v-theme-surface));
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-
-  .pomodoro-circle-component-on-pip-wrapper {
-    width: 100vmin;
-    height: 100vmin;
-    padding: 1rem;
-
-    .pomodoro-circle-component-on-pip {
-      height: 100%;
-    }
-  }
-}
-
-
-// TODO Blur
-
 
 .rounded-box {
   border-radius: 1rem;
@@ -309,12 +184,14 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
       .created-box-wrapper {
         position: relative;
       }
+
       @media (max-width: 600px) {
         .title {
           display: flex;
           flex-direction: column;
         }
       }
+
       .minecraft-sentence {
         position: absolute;
         top: 0;
@@ -324,34 +201,41 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
         transform: translate(-10%, -0em) rotate(-24deg);
         animation: breath 0.5s linear infinite alternate;
       }
+
       @keyframes breath {
         0% {
           scale: 0.9;
         }
+
         100% {
           scale: 1;
         }
       }
+
       .text-version {
         text-align: right;
         margin-right: 3em;
         margin-top: -1.8em;
         font-size: 0.9rem;
+
         @media (max-width: 600px) {
           margin-top: -1.2em;
           margin-right: 2em;
         }
       }
+
       .info-welcome {
         margin: 1rem;
         top: 0;
         right: 0;
       }
     }
+
     .finish-box {
       margin: 1rem;
       padding: 1.5rem;
       position: relative;
+
       .close-icon {
         position: absolute;
         top: 0;
@@ -410,6 +294,7 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
 
     h3 {
       font-size: 1.5rem;
+
       @media (max-width: 600px) {
         font-size: 0.8rem;
       }
@@ -454,5 +339,4 @@ const minecraftSentence = minecraftSentences.sentences[Math.floor(Math.random() 
   }
 
 }
-
 </style>
