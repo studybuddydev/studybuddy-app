3d9350771d49286d5ea5dcaf84414ff7a6ee6780
Nicola Toscan <nicolatoscan99@outlook.it>
2024-02-28 07:40:00 +0100

    pomodoto report classes

--- src/types/Pomodoro.ts
+++ src/types/Pomodoro.ts
@@ -5,7 +5,7 @@ const SECONDS_MULTIPLIER = 1000;
 const MINUTE_MULTIPLIER = 60 * SECONDS_MULTIPLIER;
 const POMO_VERSION = 3;
 
-const SHORT_POMO_THRESHOLD =  5 * MINUTE_MULTIPLIER;
+const SHORT_POMO_THRESHOLD = 5 * MINUTE_MULTIPLIER;
 const LONG_BREAK_THRESHOLD = 15 * MINUTE_MULTIPLIER;
 
 const STOPPOMODORO_TIMEOUT = 60 * MINUTE_MULTIPLIER;
@@ -21,9 +21,9 @@ export default class Pomodoro {
 
   private getNewPomodoro(pomoSettings: PomodoroSettings): PomodotoStatus {
     const free = !!pomoSettings.freeMode;
-    const totalLength   = free ? 0 : pomoSettings.totalLength * MINUTE_MULTIPLIER;
-    const breaksLength  = free ? 0 : pomoSettings.breaksLength * MINUTE_MULTIPLIER;
-    const nrOfBreaks    = free ? 0 : pomoSettings.numberOfBreak;
+    const totalLength = free ? 0 : pomoSettings.totalLength * MINUTE_MULTIPLIER;
+    const breaksLength = free ? 0 : pomoSettings.breaksLength * MINUTE_MULTIPLIER;
+    const nrOfBreaks = free ? 0 : pomoSettings.numberOfBreak;
 
     return {
       version: POMO_VERSION,
@@ -44,8 +44,8 @@ export default class Pomodoro {
 
   public getBreaks() {
     return [
-      ...(this.pomo?.breaksDone.map(x => ({...x, done: true})) ?? []) ,
-      ...(this.pomo?.breaksTodo.map(x => ({...x, done: false})) ?? [])
+      ...(this.pomo?.breaksDone.map(x => ({ ...x, done: true })) ?? []),
+      ...(this.pomo?.breaksTodo.map(x => ({ ...x, done: false })) ?? [])
     ];
   }
 
@@ -57,6 +57,61 @@ export default class Pomodoro {
     this.pomo.state = PomodoroState.STUDY;
     this.pomo.originalEnd = this.pomo.end;
     this.interval = setInterval(() => this.tick(), TICK_TIME);
+  }
+
+  public stopPomodoro(lastInteraction: number | undefined = undefined) {
+    const now = lastInteraction === undefined ? this.getNow() : this.getNow(lastInteraction);
+    this.pomo.onLongBreak = false;
+    this.pomo.endedAt = now;
+    if (this.pomo.state === PomodoroState.BREAK) {
+      const lastBreak = this.pomo.breaksDone.pop();
+      if (lastBreak) {
+        this.pomo.endedAt = lastBreak.start;
+        if (!((this.pomo.originalEnd ?? 0) > this.pomo.end)) {
+          this.pomo.end = Math.max(this.pomo.originalEnd ?? 0, lastBreak.start)
+        }
+      }
+    }
+    this.pomo.state = PomodoroState.TERMINATED;
+  }
+
+  public togglePauseStudy() {
+    if (this.pomo.state === PomodoroState.STUDY) {
+      this.pause();
+    } else if (this.pomo.state === PomodoroState.BREAK) {
+      this.study();
+    }
+  }
+
+  private pause() {
+    // adjustPomo();
+    
+    const now = this.getNow();
+    this.pomo.state = PomodoroState.BREAK;
+
+    const nextBreak = this.pomo.breaksTodo[0]; // get next break
+    if (nextBreak && 2 * nextBreak.start - (nextBreak.end ?? now) < now) {  // if next break is close to now (within one break distance)
+      nextBreak.end = now + ((nextBreak.end ?? now) - nextBreak.start); // shift break
+      nextBreak.start = now;
+      this.pomo.breaksDone.push(this.pomo.breaksTodo.shift()!); // Move break from todo to done
+    } else {
+      this.pomo.breaksDone.push({ start: now, end: now, soundStart: true, soundEnd: true });     // create new break
+    }
+  }
+
+    // start the study ending a pause 
+    private study() {
+      // adjustPomo();
+  
+      const now = this.getNow(this.pomo.startedAt);
+      this.pomo.state = PomodoroState.STUDY; // set state to study
+  
+      const lastBreak = this.pomo.breaksDone[this.pomo.breaksDone.length - 1]; // get last break and set the end 
+      lastBreak.end = now;
+    }
+
+  private getNow(now: number = Date.now()) {
+    return now - (this.pomo.startedAt ?? 0);
   }
 
   private tick() {
@@ -82,4 +137,10 @@ export default class Pomodoro {
     }
     return breaks;
   }
+
+  public getPercentage() {
+    if (this.pomo.state === PomodoroState.CREATED) return 0;
+    return 100 * Math.min(this.getNow() / this.pomo.end, 1);
+  }
+
 }
