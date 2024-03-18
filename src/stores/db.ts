ab667b5cb20d6d9b5a0cd511b8667de9256743a6
Nicola Toscan <nicolatoscan99@outlook.it>
2024-03-18 22:04:21 +0100

    add pomodori mockup

--- src/stores/db.ts
+++ src/stores/db.ts
@@ -5,29 +5,29 @@ import { defineStore } from 'pinia';
 
 function getThemes() {
   return [
-    { title: 'Forest',      palette: 'bio' },
-    { title: 'Mountain',    palette: 'nord' },
-    { title: 'Rocks',       palette: 'gptday' },
-    
-    { title: 'Space',       palette: 'gptnight' },
-    { title: 'Night',       palette: 'dark' },
-    { title: 'Aurora',      palette: 'blallo' },
+    { title: 'Forest', palette: 'bio' },
+    { title: 'Mountain', palette: 'nord' },
+    { title: 'Rocks', palette: 'gptday' },
 
-    { title: 'Ghibli',      palette: 'verdone' },
-    { title: 'LOFI',        palette: 'gptnight' },
-    { title: 'Wave',        palette: 'nord' },
+    { title: 'Space', palette: 'gptnight' },
+    { title: 'Night', palette: 'dark' },
+    { title: 'Aurora', palette: 'blallo' },
 
-    { title: 'Purple',      palette: 'purple' },
-    { title: 'Beach',       palette: 'pastel' },
-    { title: 'Vaporwave',   palette: 'vaporwave' },
-    
-    { title: 'Barbie',      palette: 'pastel' },
+    { title: 'Ghibli', palette: 'verdone' },
+    { title: 'LOFI', palette: 'gptnight' },
+    { title: 'Wave', palette: 'nord' },
+
+    { title: 'Purple', palette: 'purple' },
+    { title: 'Beach', palette: 'pastel' },
+    { title: 'Vaporwave', palette: 'vaporwave' },
+
+    { title: 'Barbie', palette: 'pastel' },
     { title: 'Oppenheimer', palette: 'gptnight' },
-    { title: 'Dune',        palette: 'desert' },
-    
-    { title: 'City',        palette: 'nord' },
-    { title: 'Fog',         palette: 'gptday' },
-    { title: 'Gandalf',     palette: 'blallo' }
+    { title: 'Dune', palette: 'desert' },
+
+    { title: 'City', palette: 'nord' },
+    { title: 'Fog', palette: 'gptday' },
+    { title: 'Gandalf', palette: 'blallo' }
 
   ].map((t) => ({ ...t, previewImg: `/images/themes/${t.title}.webp`, backgroundImg: `https://api.studybuddy.it/images/${t.title}` }));
 }
@@ -66,7 +66,34 @@ export class StudyBuddyDB extends Dexie {
       this.themes.bulkAdd(getThemes());
     })
   }
+
 }
+
+// async function addPomodoriTest(pomodori: Table<PomodoroDBO, number>) {
+//   function getRandom(n: number = 7200000) {
+//     return Math.floor(Math.random() * n);
+//   }
+//   await pomodori.clear()
+//   for (let m = 0; m < 12; m++) {
+//     for (let d = 1; d < 28; d++) {
+//       for (let i = 0; i < 50; i++) {
+//         const breaks = []
+//         let tIndex = 0;
+//         for (let b = 0; b < Math.floor(Math.random() * 5); b++) {
+//           const start = Math.floor(Math.random() * 720000) + tIndex;
+//           const end = Math.floor(Math.random() * 720000) + start;
+//           const b = { start, end }
+//           tIndex += b.end
+//           breaks.push(b)
+//         }
+//         await pomodori.add({ end: 7200000, endedAt: getRandom(),
+//           breaksDone: breaks, freeMode: false, deepWork: true,
+//           datetime: new Date(2023, m, d, getRandom(24), getRandom(60), getRandom(60), 0) })
+//       }
+//     }
+//   console.log(`Added pomodoro ${m}`)
+//   }
+// }
 
 export const useDBStore = defineStore('dbStore', () => {
   const studyBuddyDB = new StudyBuddyDB();
