45ae10c1031dfb96d01cefd55b73d97a1f4e703c
gabrielesalvi <mrgabry99@gmail.com>
2024-06-13 23:00:01 +0200

    long away popup translated

--- src/components/Zen/LongAwayPopup.vue
+++ src/components/Zen/LongAwayPopup.vue
@@ -3,15 +3,15 @@
     <v-card>
       <v-card-title>
         <v-icon>mdi-clock-time-four-outline</v-icon>
-        <span>You left a timer running?</span>
+        <span>{{ $t('longAway') }}</span>
       </v-card-title>
       <v-card-text>
-        <p>Do you want to continue it?</p>
+        <p>{{ $t('continue') }}</p>
       </v-card-text>
       <v-card-actions>
         <v-spacer />
-        <v-btn @click="continuePomodoro()">Yes</v-btn>
-        <v-btn @click="stopPomodoro()">No</v-btn>
+        <v-btn @click="continuePomodoro()">{{ $t('yes') }}</v-btn>
+        <v-btn @click="stopPomodoro()">{{ $t('no') }}</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
